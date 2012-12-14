a9b301f20e6f7344b9410b930d335d3a21e32126
nudobook <tech@nudobook.fr>
2012-12-14 14:14:13 +0100

    Moulinage du git

--- src/com/oobot/robot/JoeRobot.java
+++ src/com/oobot/robot/JoeRobot.java
@@ -1,5 +1,6 @@
 package com.oobot.robot;
 
+import robocode.HitRobotEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
@@ -63,9 +64,50 @@ public class JoeRobot extends Robot {
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
 		turnGunLeft(5);
+		
 		for(int i = 0; i< 3; i++){
-			turnGunRight(5);
 			fire(0.2);
+			while(getGunHeat() > 0){
+				turnGunRight(1);
+			}
 		}
 	}
+	
+	/***
+	 * Si on entre en collision avec un autre robot
+	 * On tourne la tourelle vers lui et on tire a puissance maximum
+	 */
+	public void onHitRobot(HitRobotEvent event){
+		
+		// Si on a la possibilite de tirer rapidement
+		
+		double whereIsIt = event.getBearing();
+		
+		// Calculer si on pourra tourner assez rapidement la tourelle pour shooter le type
+		// if(getGunHeat())
+		
+		// On tourne soit a gauche, soit a droite
+		if(whereIsIt < 0){
+			turnGunLeft(whereIsIt);
+		}else{
+			turnGunRight(whereIsIt);
+		}
+		waitAndFire(3.0);
+		//
+		// Tirer un boulet de 3
+	}
+	
+	/**
+	 * Attend que la tourelle soit prete et tire
+	 * @param firePower
+	 */
+	private void waitAndFire(double firePower){
+		
+		while(getGunHeat() > 0){
+			
+		}
+		fire(firePower);
+	}
+
+	// Methode pour savoir s'il vaut mieux tourner a gauche ou a droite pour rattraper un certain degre
 }
